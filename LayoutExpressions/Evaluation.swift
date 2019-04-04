2ee08efbc5420d03bc054c58598b1c97415cc69f
Steve Brambilla <stevebrambilla@me.com>
2019-04-03 20:33:42 -0400

    Add convenience funcs for activating constraints

--- LayoutExpressions/Evaluation.swift
+++ LayoutExpressions/Evaluation.swift
@@ -24,3 +24,27 @@ public func evaluateLayoutExpressions(_ expressions: [ExpressionProtocol]) -> [N
 		return acc + expression.evaluateAll()
 	}
 }
+
+/// Evaluates a distinct layout expression and activates the constraint.
+///
+/// The effect of this function is the same as setting the `isActive` property of the constraint to
+/// `true`.
+public func activateLayoutExpression(_ expression: DistinctExpressionType) {
+    evaluateLayoutExpression(expression).isActive = true
+}
+
+/// Evaluates a layout expression and activates its constraints.
+///
+/// The effect of this function is the same as setting the `isActive` property of the constraints to
+/// `true`.
+public func activateLayoutExpression(_ expression: ExpressionProtocol) {
+    evaluateLayoutExpression(expression).activateConstraints()
+}
+
+/// Evaluates multiple layout expressions and activates their constraints.
+///
+/// The effect of this function is the same as setting the `isActive` property of each constraint
+/// to `true`.
+public func activateLayoutExpressions(_ expressions: [ExpressionProtocol]) {
+    evaluateLayoutExpressions(expressions).activateConstraints()
+}
