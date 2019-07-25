0aa64f8b205170fd4d8483231af6b53e745afb42
Steve Brambilla <stevebrambilla@me.com>
2019-07-24 20:41:04 -0400

    Disable Obj-C API

--- Sources/LayoutExpressions/LayoutExpressions.h
+++
@@ -1,9 +0,0 @@
-//  Copyright (c) 2014 Steve Brambilla. All rights reserved.
-
-#import <Foundation/Foundation.h>
-
-//! Project version number for LayoutExpressions.
-FOUNDATION_EXPORT double LayoutExpressionsVersionNumber;
-
-//! Project version string for LayoutExpressions.
-FOUNDATION_EXPORT const unsigned char LayoutExpressionsVersionString[];
